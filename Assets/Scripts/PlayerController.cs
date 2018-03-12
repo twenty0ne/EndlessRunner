288e3149b6e21c49d378c4d47b67839b0b11a2bd
twenty0ne <twenty0ne@qq.com>
2018-03-13 00:09:11 +0800

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	public float jumpSpeed;
+
+	void Update()
+	{
+		
+	}
+}
