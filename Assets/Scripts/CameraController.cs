354c4afc4e12bb029ca86edc8824c8a4747c1ef0
twenty0ne <twenty0ne@qq.com>
2018-03-13 13:58:51 +0800

    CameraController.cs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public PlayerController player;
+
+	private Vector3 _lastPlayerPostion;
+
+	private void Start()
+	{
+		player = FindObjectOfType<PlayerController>();
+		_lastPlayerPostion = player.transform.position;
+	}
+
+	private void Update()
+	{
+		float moveDistance = player.transform.position.x - _lastPlayerPostion.x;
+
+		transform.position = new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z);
+
+		_lastPlayerPostion = player.transform.position;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
 
 	public float moveSpeed;
-	public float jumpSpeed;
+	public float jumpForce;
+	public LayerMask whatIsGround;
+
+	private Rigidbody2D _rigidbody;
+	private BoxCollider2D _collider;
+
+	[SerializeField] private bool _isGround = false;
+
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_collider = GetComponent<BoxCollider2D>();
+	}
 
 	void Update()
 	{
-		
+		_isGround = Physics2D.IsTouchingLayers(_collider, whatIsGround);
+
+		_rigidbody.velocity = new Vector2(moveSpeed, _rigidbody.velocity.y);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (_isGround)
+			{
+				_rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+			}
+		}
 	}
 }
