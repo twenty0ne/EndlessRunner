7a61612d97e15a11c153efe1071557f021a366a8
twenty0ne <twenty0ne@qq.com>
2018-03-18 10:32:42 +0800

    update

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+	public GameObject pooledObject;
+	public int pooledAmount;
+
+	private List<GameObject> pooledObjects;
+
+	void Start()
+	{
+		pooledObjects = new List<GameObject>();
+
+		for (int i = 0; i < pooledAmount; ++i) 
+		{
+			NewPooledObject ();
+		}
+	}
+
+	public GameObject GetPooledObject()
+	{
+		for (int i = 0; i < pooledObjects.Count; ++i) 
+		{
+			var obj = pooledObjects [i];
+			if (obj.activeSelf == false) 
+			{
+				return obj;
+			}
+		}
+
+		var nobj = NewPooledObject ();
+		return nobj;
+	}
+
+	private GameObject NewPooledObject()
+	{
+		var obj = Instantiate (pooledObject);
+		obj.SetActive (false);
+		pooledObjects.Add (obj);
+		return obj;
+	}
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour 
 {
-	public GameObject thePlatform;
+	public GameObject[] thePlatforms;
 	public Transform generationPoint;
 
 	public float distanceBetweenMin;
 	public float distanceBetweenMax;
 
-	private float platformWidth;
+	public ObjectPooler theObjectPooler;
+
+	private float[] platformWidths;
 
 	void Start()
 	{
-		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+		platformWidths = new float[thePlatforms.Length];
+		for (int i = 0; i < thePlatforms.Length; ++i) 
+		{
+			platformWidths [i] = thePlatforms [i].GetComponent<BoxCollider2D> ().size.x;
+		}
+
+		// platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
 	}
 
 	void Update()
@@ -22,11 +30,21 @@ public class PlatformGenerator : MonoBehaviour
 		if (transform.position.x < generationPoint.position.x) 
 		{
 			float distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+			int platformSelector = Random.Range (0, thePlatforms.Length);
 
-			transform.position = new Vector3 (transform.position.x +
-			platformWidth + distanceBetween, transform.position.y, transform.position.z);
+			transform.position = new Vector3 (transform.position.x + distanceBetween, 
+				transform.position.y, transform.position.z);
 
-			Instantiate (thePlatform, transform.position, transform.rotation);
+			Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+
+			transform.position = new Vector3 (transform.position.x + platformWidths [platformSelector], 
+				transform.position.y, transform.position.z);
+
+			/*
+			var obj = theObjectPooler.GetPooledObject();
+			obj.transform.position = transform.position;
+			// obj.transform.rotation = transform.rotation;
+			obj.SetActive (true);*/
 		}
 	}
 }
